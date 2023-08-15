507fa25dcb05b468d96915456185bed348e05eaa
rai <mj82xx@gmail.com>
2023-08-16 00:11:01 +0900

    feat: 未完了と完了済みで分ける機能を追加

--- app/src/lib/TodoList.svelte
+++ app/src/lib/TodoList.svelte
@@ -6,7 +6,19 @@
 </script>
 
 <div class="todoList">
-  {#each todos as todo}
+  <h3>未完了</h3>
+  {#each todos.filter((todo) => !todo.done) as todo}
+    <TodoItem title={todo.title} done={todo.done} />
+  {/each}
+
+  <h3>完了済み</h3>
+  {#each todos.filter((todo) => todo.done) as todo}
     <TodoItem title={todo.title} done={todo.done} />
   {/each}
 </div>
+
+<style>
+  h3 {
+    margin-top: 1rem;
+  }
+</style>
