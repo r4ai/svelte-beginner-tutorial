22f20c0521ac7e8f4202b30eba40031a332c7ccc
rai <mj82xx@gmail.com>
2023-08-15 18:47:47 +0900

    feat: MainBodyを作成

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,31 +1,12 @@
 <script lang="ts">
   import "modern-css-reset";
   import NavBar from "./lib/NavBar.svelte";
+  import MainBody from "./lib/MainBody.svelte";
 </script>
 
 <main>
   <NavBar />
-  <div>
-    <h2>ToDo List</h2>
-    <div>
-      <div>
-        <input type="checkbox" />
-        <span>ToDo 1</span>
-      </div>
-      <div>
-        <input type="checkbox" />
-        <span>ToDo 2</span>
-      </div>
-      <div>
-        <input type="checkbox" />
-        <span>ToDo 3</span>
-      </div>
-    </div>
-    <div>
-      <input type="text" placeholder="Add a new ToDo" />
-      <button>Add</button>
-    </div>
-  </div>
+  <MainBody />
 </main>
 
 <style>
---
+++ app/src/lib/MainBody.svelte
@@ -0,0 +1,31 @@
+<div class="mainBody">
+  <h2>ToDo List</h2>
+  <div>
+    <div>
+      <input type="checkbox" />
+      <span>ToDo 1</span>
+    </div>
+    <div>
+      <input type="checkbox" />
+      <span>ToDo 2</span>
+    </div>
+    <div>
+      <input type="checkbox" />
+      <span>ToDo 3</span>
+    </div>
+  </div>
+  <div>
+    <input type="text" placeholder="Add a new ToDo" />
+    <button>Add</button>
+  </div>
+</div>
+
+<style>
+  .mainBody {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    margin: 1rem;
+  }
+</style>
