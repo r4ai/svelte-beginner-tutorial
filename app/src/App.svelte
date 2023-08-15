43dcb52aba9cedf69f27ad1f60e9074f1cfa2538
rai <mj82xx@gmail.com>
2023-08-15 18:20:37 +0900

    feat: 文書構造を仮で作成

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,7 +2,30 @@
 </script>
 
 <main>
-  <h1>Hello, Svelte!</h1>
+  <a href="/">
+    <h1>Svelte ToDo</h1>
+  </a>
+  <div>
+    <h2>ToDo List</h2>
+    <div>
+      <div>
+        <input type="checkbox" />
+        <span>ToDo 1</span>
+      </div>
+      <div>
+        <input type="checkbox" />
+        <span>ToDo 2</span>
+      </div>
+      <div>
+        <input type="checkbox" />
+        <span>ToDo 3</span>
+      </div>
+    </div>
+    <div>
+      <input type="text" placeholder="Add a new ToDo" />
+      <button>Add</button>
+    </div>
+  </div>
 </main>
 
 <style>
