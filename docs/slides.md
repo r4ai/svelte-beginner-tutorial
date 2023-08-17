339ef17974a7ba246eeca51fff8066079c691bfc
rai <mj82xx@gmail.com>
2023-08-17 15:45:42 +0900

    chore: slide作成用のprojectを作成

---
+++ docs/slides.md
@@ -0,0 +1,3 @@
+# Svelte 入門
+
+Svelte で簡単な ToDo アプリを作ってみる
---
+++ docs/uno.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "unocss";
+
+export default defineConfig({
+  shortcuts: {
+    // custom the default background
+    "bg-main": "bg-white text-[#181818] dark:bg-[#121212] dark:text-[#ddd]",
+  },
+});
